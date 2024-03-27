ad5cef37e1c9fcce61a7b2f39749ad859e56557d
StudentBao <joker_eg@list.ru>
2024-03-27 20:33:50 +0500

    A2

--- test.py
+++ test.py
@@ -1,6 +1,34 @@
-class First:
-    pass
+# stack=[]
+# def push(val):
+#     stack.append(val)
+#
+# def pop():
+#     val=stack[-1]
+#     del stack[-1]
+#     return val
+#
+# push(3)
+# push(2)
+# push(1)
+#
+# print(pop())
+# print(pop())
+# print(pop())
 
-f=First()
 
-print(f)
+
+
+class Zhivot:
+    def __init__(self):
+        self.hodit="На конечностях"
+
+class Zvuk(Zhivot):
+    def __init__(self):
+        Zhivot.__init__(self)
+        self.zvuki="Мяу"
+
+koshka=Zvuk()
+
+print(koshka.zvuki)
+print(koshka.hodit)
+
